0932afd2cb5e0f245e37bb290f0ccb7d6ba8dcbb
Javier Delgado <javierdelgadogm@gmail.com>
2015-04-05 22:42:59 -0300

    [FE]
    Fixed background image behaviour

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def put_background_image(image_path)
+    "<style media='screen'>
+      html { 
+        background: url(#{image_path}) no-repeat center center; 
+        background-size: cover;
+        background-attachment: fixed;
+      }
+    </style>".html_safe
+  end
 end
