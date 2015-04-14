5710d937b723629875875804ad63554cd2ff9d37
Javier Delgado <javierdelgadogm@gmail.com>
2015-04-14 13:25:10 -0300

    [Whenever]
    Disabled task due to end of URF mode

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,7 @@
 set :environment, "production"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
-every 10.minutes do
-  rake "api:collect_urf_data"
-end
+#Disabled beacuse of end of event
+#every 10.minutes do
+  #rake "api:collect_urf_data"
+#end
